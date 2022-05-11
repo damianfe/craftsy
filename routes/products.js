f8c60b42d64048f694c5fa27156624a832542e84
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-05-11 14:39:30 -0300

    add multer

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,5 @@
 
-  
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
@@ -8,22 +8,24 @@ const multer = require('multer');
 const {cart,detail,getByCategory, search, add, store,edit, update,remove} = require('../controllers/productController');
 
 /* MULTER */
- const storage = multer.diskStorage({
-     
-  destination: function (req, file, callback) { 
-    callback(null, './public/images/avatars'); 
- }, 
- filename: function (req, file, cb) { 
-    callback(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
+const storage = multer.diskStorage({
+    destination : (req,file,callback) => {
+        callback(null,'public/images')
+    },
+    filename : (req,file,callback) => {
+        callback(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
 })
- 
+const upload = multer({
+    storage
+})
 
 /* /products */
 router
     .get('/add',add)
-    .post('/add',store)
+    .post('/add',upload.single('image'),store)
     .get('/edit/:id',edit)
-    .put('/update/:id',update)
+    .put('/update/:id',upload.single('image'),update)
     .get('/cart', cart)
     .get('/detail/:id', detail)
     .get('/category/:idCategory/:idProduct?',getByCategory)
